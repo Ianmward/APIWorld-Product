c967577f4a8e99ecb5e4c3dc6b777becf4766018
Ian Ward <ianmward@gmail.com>
2019-12-10 11:03:20 +1100

    change to podTemplate

--- Jenkinsfile.test.groovy
+++ Jenkinsfile.test.groovy
@@ -1,8 +1,17 @@
-pipeline {
-  agent {
-    kubernetes {
-      label 'myPod'
-      yaml """
+def label = "mypod-${UUID.randomUUID().toString()}"
+podTemplate(label: label, 
+  properties([
+    parameters([
+      string(name: 'VERSION', defaultValue: 'ci', description: 'The target VERSION', )
+    ])
+  ]),
+  envVars: [
+    secretEnvVar(key: 'DOCKER_USR', secretName: 'docker-store-cred', secretKey: 'username'),
+    secretEnvVar(key: 'DOCKER_PSW', secretName: 'docker-store-cred', secretKey: 'password'),
+    secretEnvVar(key: 'NEXUS_USR', secretName: 'docker-nexus-cred', secretKey: 'username'),
+    secretEnvVar(key: 'NEXUS_PSW', secretName: 'docker-nexus-cred', secretKey: 'password')
+  ],
+  yaml: """
   kind: Pod
   metadata:
     labels:
@@ -31,33 +40,46 @@ pipeline {
       - mountPath: /var/run/docker.sock
         name: docker-sock-volume
 """
+) {
+  node(label) {
+    def commitId
+		stage ('test-docker') {
+            container('docker') {
+                sh "echo $commitId"
+                sh "id"
+                sh "ls -l /var/run/docker.sock"
+			}
+		}
+    stage ('Extract') {
+        checkout scm
+        commitId = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
     }
-  }
-  stages {
-      stage('Test Operational') {
-          failFast true
-          parallel {
-              stage('Test MicroGW Operational') {
-                  steps {
-                      container('mg-jenkins') {
-                          sh '''
+    def repository
+
+    stage('Test Operational') {
+        failFast true
+        parallel {
+            stage('Test MicroGW Operational') {
+                steps {
+                    container('mg-jenkins') {
+                        sh '''
 #Is MicroGW Operational
 timeout 60 bash -c \'while [[ "$(curl -s -o /dev/null -w \'\'%{http_code}\'\' localhost:9090/gateway/Product/1.0/product)" != "200" ]]; do sleep 5; done\' || false
 '''
-                      }
-                  }
-              }
-              stage('Test MicroSvc Operational') {
-                  steps {
-                      container('mg-jenkins') {
-                          sh '''
+                    }
+                }
+            }
+            stage('Test MicroSvc Operational') {
+                steps {
+                    container('mg-jenkins') {
+                        sh '''
 #Are services operational
 timeout 60 bash -c \'while [[ "$(curl -s -o /dev/null -w \'\'%{http_code}\'\' localhost:8090/product)" != "200" ]]; do sleep 5; done\' || false
 '''
-                      }
-                  }
-              }
-          }
-      }
+                    }
+                }
+            }
+        }
+    }
   }
 }
